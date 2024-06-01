79002ea7df5b2ea07946b42e70c24ff715c50917
fujivara <marat.skyba@gmail.com>
2024-06-01 23:33:50 +0300

    balancer implementation

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -14,19 +15,19 @@ import (
 )
 
 var (
-	port = flag.Int("port", 8090, "load balancer port")
+	port       = flag.Int("port", 8090, "load balancer port")
 	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
-	https = flag.Bool("https", false, "whether backends support HTTPs")
+	https      = flag.Bool("https", false, "whether backends support HTTPs")
 
 	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 )
 
 var (
-	timeout = time.Duration(*timeoutSec) * time.Second
-	serversPool = []string{
-		"server1:8080",
-		"server2:8080",
-		"server3:8080",
+	timeout     = time.Duration(*timeoutSec) * time.Second
+	serversPool = map[string]int{
+		"server1:8080": 0,
+		"server2:8080": 0,
+		"server3:8080": 0,
 	}
 )
 
@@ -51,6 +52,15 @@ func health(dst string) bool {
 	return true
 }
 
+func updateServerBytes(bytes int, server string) {
+	serversPool[server] += bytes
+}
+
+func countResponseBytes(res *http.Response) int {
+	body, _ := ioutil.ReadAll(res.Body)
+	return len(body)
+}
+
 func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 	ctx, _ := context.WithTimeout(r.Context(), timeout)
 	fwdRequest := r.Clone(ctx)
@@ -72,6 +82,9 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 		log.Println("fwd", resp.StatusCode, resp.Request.URL)
 		rw.WriteHeader(resp.StatusCode)
 		defer resp.Body.Close()
+
+		updateServerBytes(countResponseBytes(resp), dst)
+
 		_, err := io.Copy(rw, resp.Body)
 		if err != nil {
 			log.Printf("Failed to write response: %s", err)
@@ -84,11 +97,24 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 	}
 }
 
+func getServer() string {
+	var minServer string
+	minValue := int(^uint(0) >> 1) // Initialize with maximum int value
+
+	for server, value := range serversPool {
+		if value < minValue {
+			minValue = value
+			minServer = server
+		}
+	}
+	return minServer
+}
+
 func main() {
 	flag.Parse()
 
 	// TODO: Використовуйте дані про стан сервреа, щоб підтримувати список тих серверів, яким можна відправляти ззапит.
-	for _, server := range serversPool {
+	for _, server := range make([]string, 0, len(serversPool)) {
 		server := server
 		go func() {
 			for range time.Tick(10 * time.Second) {
@@ -98,8 +124,11 @@ func main() {
 	}
 
 	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		// TODO: Рееалізуйте свій алгоритм балансувальника.
-		forward(serversPool[0], rw, r)
+		serverName := getServer()
+		err := forward(serverName, rw, r)
+		if err != nil {
+			return
+		}
 	}))
 
 	log.Println("Starting load balancer...")
