1e450a8b77459f10a03a23d0e1953221e7c8f232
fujivara <marat.skyba@gmail.com>
2024-06-02 14:36:39 +0300

    integration testing upd

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,100 +1,44 @@
 package integration
 
 import (
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-	_ "time"
-
-	"github.com/jarcoal/httpmock"
-	"github.com/roman-mazur/architecture-practice-4-template/cmd/lb"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	_ "net/http"
+	"os"
+	"testing"
+	"time"
+	_ "time"
 )
 
-func TestLoadBalancingDistribution(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
+var client = http.Client{
+	Timeout: 3 * time.Second,
+}
 
-	balancer.ServersPool = map[string]int{
-		"server1:8080": 0,
-		"server2:8080": 0,
-		"server3:8080": 0,
+func TestLoadBalancerAlgorithm(t *testing.T) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		t.Skip("Integration test is not enabled")
 	}
 
-	httpmock.RegisterResponder("GET", "http://server1:8080/test",
-		httpmock.NewStringResponder(200, "Server1 response"))
-	httpmock.RegisterResponder("GET", "http://server2:8080/test",
-		httpmock.NewStringResponder(200, "Server2 response"))
-	httpmock.RegisterResponder("GET", "http://server3:8080/test",
-		httpmock.NewStringResponder(200, "Server3 response"))
-
-	req := httptest.NewRequest("GET", "/test", nil)
-	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		serverName := balancer.GetServer()
-		err := balancer.Forward(serverName, rw, r)
-		if err != nil {
-			return
-		}
-	})
+	loadBalancerAddress := "http://balancer:8090"
 
 	numRequests := 10
-	serverResponses := make(map[string]int)
+	serverResponsesCount := make(map[string]int)
 
 	for i := 0; i < numRequests; i++ {
-		w := httptest.NewRecorder()
-		handler.ServeHTTP(w, req)
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", loadBalancerAddress))
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
 
-		resp := w.Result()
-		body, _ := ioutil.ReadAll(resp.Body)
-
-		serverResponses[string(body)]++
+		serverResponsesCount[resp.Header.Get("lb-from")]++
 	}
 
-	assert.Greater(t, len(serverResponses), 1, "Responses should come from more than one server")
-	for server, count := range serverResponses {
+	assert.Greater(t, len(serverResponsesCount), 1, "Responses should come from more than one server")
+	for server, count := range serverResponsesCount {
 		t.Logf("Server %s handled %d requests", server, count)
 	}
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	balancer.ServersPool = map[string]int{
-		"server1:8080": 0,
-		"server2:8080": 0,
-		"server3:8080": 0,
-	}
-
-	httpmock.RegisterResponder("GET", "http://server1:8080/test",
-		httpmock.NewStringResponder(200, "Server1 response"))
-	httpmock.RegisterResponder("GET", "http://server2:8080/test",
-		httpmock.NewStringResponder(200, "Server2 response"))
-	httpmock.RegisterResponder("GET", "http://server3:8080/test",
-		httpmock.NewStringResponder(200, "Server3 response"))
-
-	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		serverName := balancer.GetServer()
-		err := balancer.Forward(serverName, rw, r)
-		if err != nil {
-			return
-		}
-	})
-
-	req := httptest.NewRequest("GET", "/test", nil)
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		w := httptest.NewRecorder()
-		handler.ServeHTTP(w, req)
-
-		resp := w.Result()
-		_, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			b.Fatal(err)
-		}
-		resp.Body.Close()
-	}
-}
+func BenchmarkBalancer(b *testing.B) {}
